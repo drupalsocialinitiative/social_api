ca65f4c9ef0d8b39d72b5fd04185c42b03d0e4c9
Getulio Sánchez <valentin2507@gmail.com>
2016-04-28 15:37:03 -0400

    Separate Social API project into 4 modules

--- src/Controller/SocialApiController.php
+++ src/Controller/SocialApiController.php
@@ -15,11 +15,6 @@ use Drupal\social_api\Plugin\NetworkManager;
 class SocialApiController extends ControllerBase
 {
   /**
-   * @var LocalTaskManager
-   */
-  private $localTaskManager;
-
-  /**
    * @var NetworkManager
    */
   private $networkManager;
@@ -28,44 +23,16 @@ class SocialApiController extends ControllerBase
    * @inheritdoc
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('plugin.manager.menu.local_task'),
-                      $container->get('plugin.network.manager'));
+    return new static($container->get('plugin.network.manager'));
   }
+
 
   /**
    * SocialApiController constructor.
-   * @param LocalTaskManager $localTaskManager
+   * @param NetworkManager $networkManager
    */
-  public function __construct(LocalTaskManager $localTaskManager, NetworkManager $networkManager) {
-    $this->localTaskManager = $localTaskManager;
+  public function __construct(NetworkManager $networkManager) {
     $this->networkManager = $networkManager;
-  }
-
-  /**
-   * Render the list of plugins for a social network
-   *
-   * @param string $route
-   * @return array
-   */
-  public function localTaskList($route) {
-    $build = [
-      '#theme' => 'local_task_list',
-    ];
-
-    $taskList = $this->localTaskManager->getDefinitions();
-
-    $items = array();
-
-    foreach($taskList as $taskName => $task) {
-      if($task['base_route'] == $route) {
-        $items[$taskName]['route_name'] = $task['route_name'];
-        $items[$taskName]['title'] = $task['title']->render();
-      }
-    }
-
-    $build['#items'] = $items;
-
-    return $build;
   }
 
   /**
@@ -96,34 +63,4 @@ class SocialApiController extends ControllerBase
       '#empty' => $this->t('There are no social integrations enabled.'),
     ];
   }
-
-  /**
-   * Set the settings for the login button for the given social networking
-   *
-   * @param $module
-   * @param $route
-   * @param $img_path
-   */
-  public static function setLoginButtonSettings($module, $route, $img_path) {
-    $config = \Drupal::configFactory()->getEditable('social_api.settings');
-
-    $img_path = drupal_get_path('module', $module) . '/' . $img_path;
-
-    $config->set('auth.' . $module . '.route', $route)
-      ->set('auth.' . $module . '.img_path', $img_path)
-      ->save();
-  }
-
-  /**
-   * Delete the settings for the login button for the given social networking
-   *
-   * @param $module
-   */
-  public static function deleteLoginButtonSettings($module) {
-    $config = \Drupal::configFactory()->getEditable('social_api.settings');;
-
-    $config->clear('auth.' . $module)
-      ->save();
-  }
-  
 }
