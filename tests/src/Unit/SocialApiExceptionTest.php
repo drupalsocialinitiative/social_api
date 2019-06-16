ffc81a4963817e55a78b33fd01e9c62944a8cafa
neerajpandey <neerajyednap@gmail.com>
2019-06-16 12:09:08 +0530

    fix coding standard violations in SocialApiException class

--- tests/src/Unit/SocialApiExceptionTest.php
+++ tests/src/Unit/SocialApiExceptionTest.php
@@ -3,19 +3,26 @@
 use Drupal\Tests\UnitTestCase;
 use Drupal\social_api\SocialApiException;
 
-class SocialApiExceptionTest extends UnitTestCase{
+/**
+ * Defines class for SocialApiExcetion Test.
+ */
+class SocialApiExceptionTest extends UnitTestCase {
+
+  /**
+   * Tests for class SocialApiException.
+   */
   public function testException() {
     $socialApiException = new SocialApiException();
     try {
-      if (!$socialApiException)
-      throw new Exception();
-    } catch(\Exception $e) {
-      echo 'Message: ' .$e;
+      if (!$socialApiException) {
+        throw new Exception();
+      }
     }
-    //we need some assertion here otherwise the test will show a warning.
+    catch (\Exception $e) {
+      echo 'Message: ' . $e;
+    }
+    // We need some assertion here otherwise the test will show a warning.
     $this->assertTrue($socialApiException instanceof SocialApiException);
   }
 
 }
-
- ?>
