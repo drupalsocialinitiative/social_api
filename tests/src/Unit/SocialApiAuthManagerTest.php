e4a8f63c7d263709fa14ed9d738ce461069f10b1
neerajp99 <neerajp1999@gmail.com>
2019-06-21 20:52:41 +0530

    Edit tests

---
+++ tests/src/Unit/SocialApiAuthManagerTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use Drupal\social_api\AuthManager\OAuth2Manager;
+use Drupal\social_api\AuthManager\OAuth2ManagerInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Defines OAuth2Manager.
+ *
+ * @Annotation
+ */
+class SocialApiAuthManagerTest extends UnitTestCase {
+
+  /**
+   * Tests for class OAuth2Manager.
+   */
+  public function testOAuth2Manager() {
+    $authManager = $this->getMockBuilder(OAuth2Manager::class)
+      ->getMockForAbstractClass();
+
+    $this->assertTrue(
+      method_exists($authManager, 'setClient'),
+        'OAuth2Manager class does not implements setClient function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManager, 'getClient'),
+        'OAuth2Manager class does not implements getClient function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManager, 'setAccessToken'),
+        'OAuth2Manager class does not implements setAccessToken function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManager, 'getAccessToken'),
+        'OAuth2Manager class does not implements getAccessToken function/method'
+      );
+
+    $authManager->setClient('drupal12345');
+
+    $authManager->setAccessToken('drupal12345');
+
+    $this->assertEquals('drupal12345', $authManager->getClient());
+    $this->assertEquals('drupal12345', $authManager->getAccessToken());
+  }
+
+  /**
+   * Tests for class OAuth2ManagerInterface.
+   */
+  public function testOAuth2ManagerInterface() {
+    $authManagerInterface = $this->createMock(OAuth2ManagerInterface::class);
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'setClient'),
+        'OAuth2ManagerInterface does not have setClient function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'getClient'),
+        'OAuth2ManagerInterface does not have getClient function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'getAccessToken'),
+        'OAuth2ManagerInterface does not have getAccessToken function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'setAccessToken'),
+        'OAuth2ManagerInterface does not have setAccessToken function/method'
+      );
+
+    $this->assertTrue(
+    method_exists($authManagerInterface, 'getAuthorizationUrl'),
+        'OAuth2ManagerInterface does not have getAuthorizationUrl function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'getState'),
+        'OAuth2ManagerInterface does not have getState function/method'
+      );
+
+    $this->assertTrue(
+      method_exists($authManagerInterface, 'getUserInfo'),
+        'OAuth2ManagerInterface does not have getUserInfo function/method'
+      );
+  }
+
+}
---
+++ tests/src/Unit/SocialApiUserTest.php
@@ -0,0 +1,128 @@
+<?php
+
+use Drupal\social_api\User\UserAuthenticator;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\social_api\SocialApiDataHandler;
+use Drupal\social_api\User\UserManagerInterface;
+use Drupal\social_api\User\UserManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Defines a User class.
+ *
+ * @Annotation
+ */
+class SocialApiUserTest extends UnitTestCase {
+
+  /**
+   * Tests for class UserAuthenticator.
+   */
+  public function testUserAuthenticator() {
+    $sessionKeys = ['drupal', 'drupal234', '1234'];
+    $current_user = $this->createMock(AccountProxyInterface::class);
+    $messenger = $this->createMock(MessengerInterface::class);
+    $logger_factory = $this->createMock(LoggerChannelFactoryInterface::class);
+    $user_manager = $this->createMock(UserManagerInterface::class);
+    $data_handler = $this->createMock(SocialApiDataHandler::class);
+
+    $userAuthenticator = $this->getMockBuilder(UserAuthenticator::class)
+      ->setConstructorArgs([$current_user,
+        $messenger,
+        $logger_factory,
+        $user_manager,
+        $data_handler,
+      ])
+      ->setMethods(null)
+      ->getMockForAbstractClass();
+
+    $this->assertTrue(
+      method_exists($userAuthenticator, 'setPluginId'),
+      'UserAuthenticator does not implements setPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userAuthenticator, 'getPluginId'),
+      'UserAuthenticator does not implements getPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userAuthenticator, 'setSessionKeysToNullify'),
+      'UserAuthenticator does not implements setSessionKeysToNullify function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userAuthenticator, 'nullifySessionKeys'),
+      'UserAuthenticator does not implements nullifySessionKeys function/method'
+    );
+
+    $userAuthenticator->setPluginId('drupal123');
+    $userAuthenticator->setSessionKeysToNullify(['session123']);
+
+    $this->assertEquals('drupal123', $userAuthenticator->getPluginId());
+  }
+
+  /**
+   * Tests for class UserManager.
+   */
+  public function testUserManager() {
+    $entity_type_manager = $this->createMock(EntityTypeManagerInterface::class);
+    $messenger = $this->createMock(MessengerInterface::class);
+    $logger_factory = $this->createMock(LoggerChannelFactoryInterface::class);
+
+    $userManager = $this->getMockBuilder(UserManager::class)
+      ->setConstructorArgs(["drupalentity",
+        $entity_type_manager,
+        $messenger,
+        $logger_factory,
+      ])
+      ->setMethods(null)
+      ->getMockForAbstractClass();
+
+    $this->assertTrue(
+      method_exists($userManager, 'setPluginId'),
+      'UserManager does not implements setPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userManager, 'getPluginId'),
+      'UserManager does not implements getPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userManager, 'getDrupalUserId'),
+      'UserManager does not implements getDrupalUserId function/method'
+    );
+
+    $userManager->setPluginId('drupal123');
+
+    $this->assertEquals('drupal123', $userManager->getPluginId());
+  }
+
+  /**
+   * Tests for class UserManagerInterface.
+   */
+  public function testUserManagerInterface() {
+    $userManagerInterface = $this->createMock(UserManagerInterface::class);
+
+    $this->assertTrue($userManagerInterface instanceof UserManagerInterface);
+
+    $this->assertTrue(
+      method_exists($userManagerInterface, 'getPluginId'),
+      'UserManagerInterface does not implements getPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userManagerInterface, 'setPluginId'),
+      'UserManagerInterface does not implements fsetPluginId function/method'
+    );
+
+    $this->assertTrue(
+      method_exists($userManagerInterface, 'getDrupalUserId'),
+      'UserManagerInterface does not implements getDrupalUserId function/method'
+    );
+  }
+
+}
